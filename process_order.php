b15408c35d705de1734413b95d18e7f23bcedb76
StartledHamster <110441979+StartledHamster@users.noreply.github.com>
2022-10-18 13:14:31 +1100

    Create process_order.php

---
+++ process_order.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="styles/style.css" rel="stylesheet"/>   
+    <link href="styles/style_enquire.css" rel="stylesheet"/>
+    <meta charset="utf-8" />
+    <meta name="description" content="" />
+    <meta name="keywords" 	 content="HTML, CSS, Javascript" />
+    <meta name="author"		 content="" />
+    <title>Enquire</title>
+</head>
+<body>
+  <?php
+    
+  ?>
+</body>
+</html>
