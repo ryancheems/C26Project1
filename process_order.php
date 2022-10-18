ad5614f67b5351eafa52d0c3503d70548def2ec1
StartledHamster <110441979+StartledHamster@users.noreply.github.com>
2022-10-18 14:27:23 +1100

    Update process_order.php

--- process_order.php
+++ process_order.php
@@ -1,17 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <link href="styles/style.css" rel="stylesheet"/>   
-    <link href="styles/style_enquire.css" rel="stylesheet"/>
-    <meta charset="utf-8" />
-    <meta name="description" content="" />
-    <meta name="keywords" 	 content="HTML, CSS, Javascript" />
-    <meta name="author"		 content="" />
-    <title>Enquire</title>
-</head>
-<body>
-  <?php
+<?php
+    function sanitise_input ($data) {
+		$data = trim($data);
+		$data = stripslashes($data);
+		$data = htmlspecialchars($data);
+		return $data;
+	}
     
-  ?>
-</body>
-</html>
+?>
+
