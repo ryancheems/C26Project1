03d7373182847d2797184f948ae40f8f1e34dc14
BeemoNgo <110442099+BeemoNgo@users.noreply.github.com>
2022-10-27 05:41:31 +1100

    Update receipt.php

--- receipt.php
+++ receipt.php
@@ -2,7 +2,7 @@
     echo "<h1>Your Receipt</h1>";
     echo ("<p>Your name is $fname $lname</p>");
     echo ("<p>Your email is $email</p>");
-    echo ("<p> Address: $add, $st, $state, $pc</p>");
+    echo ("<p>Address: $add, $st, $state, $pc</p>");
     echo ("<p>Phone number: $phone</p>");
     echo ("<p>Preferred contact: $contact</p>");
     echo ("<p>The product you want to order: $product</p>");
