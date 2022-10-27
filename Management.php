764e66c042f80ebe309b7a34f74c7a36675357f2
nguyenchungbaoan <110442039+nguyenchungbaoan@users.noreply.github.com>
2022-10-27 14:23:47 +1100

    Update Management.php

--- Management.php
+++ Management.php
@@ -1,1 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="styles/style.css" rel="stylesheet"/>   
+    <link href="styles/style_enquire.css" rel="stylesheet"/>
+    <meta charset="utf-8" />
+    <meta name="description" content="Enquire" />
+    <meta name="keywords" 	 content="HTML, CSS, Javascript" />
+    <meta name="author"		 content="Thao Ngan Ngo" />
+    <title>Enquire</title>
+</head>
+<body>
 
+     	<?php include 'includes/header.inc'; ?>
