4b8c2b1d8f9ada3b075ffddb85a7052a16790167
StartledHamster <110441979+StartledHamster@users.noreply.github.com>
2022-10-27 17:01:19 +1100

    Update receipt.php

--- receipt.php
+++ receipt.php
@@ -9,45 +9,21 @@
 <body>
 <h1>Receipt</h1>
 <?php
-    require_once "settings.php";
-    $conn = @mysqli_connect($host,$user,$pwd,$sql_db);
-    if (!$conn) {
-        // Displays an error message
-        echo "<p>Database connection failure</p>";
-    } else {
-        // Upon successful connection
-        $sql_table="Order";
-                // Set up the SQL command to query or add data into the table
-                $query = "SELECT  make, model, price FROM Order ORDER BY make, model";
+    session_start();
+    $fname =  $_SESSION['fname'];
+    $lname = $_SESSION['lname'] ;
+    $email = $_SESSION['email'] ;
+    $sadd = $_SESSION['address'];
+    $st = $_SESSION['suburb'] ;
+    $state = $_SESSION['state'];
+    $pc = $_SESSION['postcode'];
+    $phone = $_SESSION['phone'];
+    $contact = $_SESSION['contact'];
+    $product = $_SESSION['product'];
+    $features = $_SESSION['features'];
+    $ctype = $_SESSION['ctype'];
+    $cnum = $_SESSION['cnum'];
 
-                // Execute the query and store result into the result pointer
-                $result = mysqli_query($conn, $query);
-
-                // Checks if the execution was successful
-                if (!$result) {
-                    echo "<p>Something is wrong with ", $query, "</p>";
-                } else {
-                    //Display the retrieved records
-                        echo "<table border=\"1\">\n";
-                        echo "<tr>\n "
-                            ."<th scope=\"col\">Order_id</th>\n "
-                            ."<th scope=\"col\">Order_status</th>\n "
-                            ."</tr>\n ";
-                        while ($row = mysqli_fetch_assoc($result)){
-                            echo "<tr>\n ";
-                            echo "<td>",$row["order_id"],"</td>\n ";
-                            echo "<td>",$row["order_status"],"</td>\n ";
-                            echo "</tr>\n ";
-                        }
-
-                    echo "</table>\n ";
-                // Frees up the memory, after using the result pointer
-                    mysqli_free_result($result);
-                } // if successful query operation
-
-                // close the database connection
-                mysqli_close($conn);
-            } //if successful database connection
     echo ("<p>Your name is $fname $lname</p>");
     echo ("<p>Your email is $email</p>");
     echo ("<p> Address: $add, $st, $state, $pc</p>");
@@ -55,6 +31,12 @@
     echo ("<p>Preferred contact: $contact</p>");
     echo ("<p>The product you want to order: $product</p>");
     echo ("<p>Product features: $features</p>");
+    echo ("<p>$ctype</p>");
+    echo ("<p>$cnum</p>");
+    echo ("<p>$cname</p>");
+    echo ("<p>$cexp</p>");
+    echo ("<p>$ccsc</p>");
+
 ?>
 </body>
 </html>
