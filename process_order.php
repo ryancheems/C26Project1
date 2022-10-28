e3455449b7f78707f85f2c78e77f10daedd4689f
StartledHamster <110441979+StartledHamster@users.noreply.github.com>
2022-10-28 18:07:09 +1100

    Update process_order.php

--- process_order.php
+++ process_order.php
@@ -174,7 +174,7 @@
     $ccsc = sanitise_input($ccsc);
     $quantity = sanitise_input($quantity);
 
-	$errors = array("","","","","","","","","","","","");
+	$errors = array("","","","","","","","","","","","","");
     $errMsg = "";
     if ($fname=="") {
         $errMsg .= "<p>You must enter your first name.</p>";
