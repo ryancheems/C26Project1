dd4cc80607d8b3bca091039d91e896e9c645da25
StartledHamster <110441979+StartledHamster@users.noreply.github.com>
2022-10-29 12:36:36 +1100

    Update Management.php

--- Management.php
+++ Management.php
@@ -11,16 +11,20 @@
 <body>
     <?php include 'includes/header.inc'; ?>
     <?php 
-    $buttonvar = "SELECT * FROM `orders`";
+    session_start();
+    if (!(isset($_SESSION["buttonvar"]))){
+        $buttonvar = "SELECT * FROM `orders`";
+        $_SESSION["buttonvar"] = $buttonvar;
+    }
     echo "<table border = '1'>";
-    echo "<tr><th>Order ID</th><th>Order Status</th><th>First Name</th><th>Last Name</th>
+    echo "<tr><th>Order ID</th><th>Order Status</th><th>Order Date</th><th>First Name</th><th>Last Name</th>
     <th>Email</th><th>Address</th><th>Suburb</th><th>State</th><th>Postcode</th><th>Phone</th><th>Contact</th>
     <th>Product</th><th>Features</th><th>Quantity</th><th>Cost</th></tr>";
     require_once "settings.php";
     $conn = mysqli_connect ($host, $user, $pwd, $sql_db);
     if ($conn) {
         echo "<p>Connection successful!</p>";
-        $query = $buttonvar;
+        $query = $_SESSION["buttonvar"];
         $queryResult = mysqli_query($conn, $query);
         if ($queryResult){
             $row = mysqli_fetch_row($queryResult);
@@ -38,8 +42,9 @@
                 echo "<td>$row[10]</td>";
                 echo "<td>$row[11]</td>";
                 echo "<td>$row[12]</td>";
-                echo "<td>$row[18]</td>";
+                echo "<td>$row[13]</td>";
                 echo "<td>$row[19]</td>";
+                echo "<td>$row[20]</td>";
                 echo "<td><form method='post'><input type='submit' name='tbutt1' id='tbutt' value='Edit'></form></td>";
                 if (($row[1])=='PENDING'){
                     echo "<td><form method='post'><input type='submit' name='tbutt2' id='tbutt2' value='Cancel'></form></td></tr>";
@@ -80,14 +85,17 @@
         }
          if (isset($_POST["bname"])){
             $bname = ($_POST["bname"]);
+             $_SESSION["name"] = $bname;
             echo  $_POST['bname'];
         }
         function button1() {
             $buttonvar = "SELECT * FROM `orders`";
+            $_SESSION["buttonvar"] = $buttonvar;
             echo "Selected All";
         }
         function button2() {
-            $buttonvar = "SELECT * FROM `orders` WHERE fname == $bname";
+            $buttonvar = "SELECT * FROM `orders` WHERE fname == $_SESSION["name"]";
+            $_SESSION["buttonvar"] = $buttonvar;
             echo 
             "<form method='post'>
             <input type='text' name='bname' id='bname' value='Enter Name'>
@@ -96,7 +104,8 @@
         }
         
         function button3() {
-            $buttonvar = "SELECT * FROM `orders` WHERE product == $bproduct";
+            $buttonvar = "SELECT * FROM `orders` WHERE product == $_SESSION["product"]";
+            $_SESSION["buttonvar"] = $buttonvar;
             echo 
             "<form method='post'>
             <input type='text' name='bproduct' id='bproduct' value='Enter Product Type'>
@@ -104,16 +113,19 @@
         }
         if (isset($_POST["bproduct"])){
             $bproduct = ($_POST["bproduct"]);
+            $_SESSION["product"] = $product;
             echo  $_POST['bproduct'];
         }
         function button4() {
             // change this
-            $buttonvar = "SELECT * FROM `orders` WHERE order_status == 'Pending'";
+            $buttonvar = "SELECT * FROM `orders` WHERE order_status == 'PENDING'";
+            $_SESSION["buttonvar"] = $buttonvar;
             echo "Pending";
         }
         function button5() {
             // change this
             $buttonvar = "SELECT * FROM `orders` ORDER BY  order_cost DESC";
+            $_SESSION["buttonvar"] = $buttonvar;
             echo "Price";
         }
         function tbutt1() {
